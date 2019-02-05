992bf888ee9e489ba139a0ea45b6884c133fc1e5
Dan Hicks <hicks.daniel.j@gmail.com>
2019-02-05 13:08:26 -0800

    filling in script 02

--- scripts/02_Scopus_search_results.R
+++ scripts/02_Scopus_search_results.R
@@ -50,9 +50,10 @@ scrape = function (this_eid, target_folder) {
 # scrape(eids[1], pub_folder)
 
 ## Do the scraping ----
+## 85 sec / 100 records -> ~20k sec = 5.5 hours
 tic()
 pub_files = eids %>% 
-    head(100) %>% 
+    # head(2e4) %>% 
     future_map_chr(scrape, target_folder = pub_folder, 
                    .progress = TRUE)
 toc()
@@ -103,44 +104,79 @@ parse_ = function (raw) {
                       subject_name, 
                       subject_area)
     
-    author_nodes = xml %>%
-        xml_find_all('//authors/author')
+    ## This code is simpler, but only gets the high-level affiliation
+    ## ie, only UCD
+    # author_nodes = xml %>%
+    #     xml_find_all('//authors/author')
+    # auids = author_nodes %>%
+    #     xml_attr('auid')
+    # surnames = author_nodes %>%
+    #     xml_find_first('ce:surname') %>%
+    #     xml_text()
+    # given_names = author_nodes %>%
+    #     xml_find_first('ce:given-name') %>%
+    #     xml_text()
+    # 
+    # if (length(author_nodes) > 0) {
+    #     affiliation_map = author_nodes %>%
+    #         `names<-`(auids) %>%
+    #         map(xml_find_all, './/affiliation') %>%
+    #         map(xml_attr, 'id') %>%
+    #         map(~ tibble(aff_id = .)) %>%
+    #         bind_rows(.id = 'auid')
+    #     
+    #     affiliations = xml %>%
+    #         xml_find_all('./affiliation')
+    #     aff_ids = affiliations %>%
+    #         xml_attr('id')
+    #     aff_names = affiliations %>%
+    #         xml_find_first('affilname') %>%
+    #         xml_text()
+    #     affiliation_key = tibble(aff_id = aff_ids, 
+    #                              aff_name = aff_names)
+    #     
+    #     aff_tbl = full_join(affiliation_map, affiliation_key)
+    # } else {
+    #     aff_tbl = tibble(auid = character())
+    # }
+    # 
+    # authors = tibble(auid = auids, 
+    #                  surname = surnames, 
+    #                  given_name = given_names) %>% 
+    #     left_join(aff_tbl)
+    
+    ## This approach gives us the affiliation as given in the paper
+    author_groups = xml %>% 
+        xml_find_all('//author-group') %>% 
+        `names<-`(., 1:length(.))
+    ## Extract affiliations and build author group-affiliation map
+    aff_names = author_groups %>% 
+        map(xml_find_all, './/affiliation') %>% 
+        map(xml_find_first, './organization') %>% 
+        map(xml_text)
+    aff_ids = author_groups %>% 
+        map(xml_find_all, './/affiliation') %>% 
+        map(xml_attr, 'afid')
+    aff_map = list(aff_name = aff_names, aff_id = aff_ids) %>% 
+        transpose() %>% 
+        map_dfr(as_tibble, .id = 'author_group')
+    ## Extract authors
+    author_nodes = author_groups %>% 
+        map(xml_find_all, './/author')
     auids = author_nodes %>%
-        xml_attr('auid')
+        map(xml_attr, 'auid')
     surnames = author_nodes %>%
-        xml_find_first('ce:surname') %>%
-        xml_text()
+        map(xml_find_first, 'ce:surname') %>%
+        map(xml_text)
     given_names = author_nodes %>%
-        xml_find_first('ce:given-name') %>%
-        xml_text()
-    
-    if (length(author_nodes) > 0) {
-        affiliation_map = author_nodes %>%
-            `names<-`(auids) %>%
-            map(xml_find_all, './/affiliation') %>%
-            map(xml_attr, 'id') %>%
-            map(~ tibble(aff_id = .)) %>%
-            bind_rows(.id = 'auid')
-        
-        affiliations = xml %>%
-            xml_find_all('./affiliation')
-        aff_ids = affiliations %>%
-            xml_attr('id')
-        aff_names = affiliations %>%
-            xml_find_first('affilname') %>%
-            xml_text()
-        affiliation_key = tibble(aff_id = aff_ids, 
-                                 aff_name = aff_names)
-        
-        aff_tbl = full_join(affiliation_map, affiliation_key)
-    } else {
-        aff_tbl = tibble(auid = character())
-    }
-    
-    authors = tibble(auid = auids, 
-                     surname = surnames, 
-                     given_name = given_names) %>% 
-        left_join(aff_tbl)
+        map(xml_find_first, 'ce:given-name') %>%
+        map(xml_text)
+    ## Combine w/ affiliations
+    authors = list(auid = auids, surname = surnames, 
+                      given_name = given_names) %>% 
+        transpose() %>% 
+        map_dfr(as_tibble, .id = 'author_group') %>% 
+        left_join(aff_map)
     
     references = xml %>%
         xml_find_all('//bibliography/reference') %>%
@@ -152,17 +188,48 @@ parse_ = function (raw) {
            subjects = list(subjects),
            references = list(references))
 }
-# parse_(this_raw) %>% 
-#     unnest(authors) %>% 
-#     View
+
 parse = function(target_file) {
+    print(target_file)
     raw = read_file(target_file)
-    return(parse_(raw))
+    if (raw == '') {
+        ## Handle empty responses
+        scopus_id = str_extract(target_file, '[0-9]{8,}')
+        return(tibble(scopus_id = scopus_id))
+    } else {
+        return(parse_(raw))
+    }
 }
 
-# parse(pub_files[1])
+# parse('../data/docs/2-s2.0-85007135357.xml') %>% 
+#     unnest(authors) %>% 
+#     select(surname, aff_id, aff_name)
+
+## 46 sec/100 files -> ~3 hours
+## 7785.614 sec = 2+ hours
 tic()
 pubs = pub_files %>% 
-    # head(50) %>%
     future_map_dfr(parse, .progress = TRUE)
 toc()
+
+pubs %>% 
+    filter(!is.na(doi)) %>% 
+    unnest(authors) %>% 
+    count(aff_id) %>% 
+    arrange(desc(n))
+
+ucd_ids = c('60014439', '60023317', '60003160', '60072478', '60000000', 
+            '60022586', '60114314', '60114420', '60110341', '60086298', 
+            '60028407')
+
+pubs %>% 
+    filter(!is.na(doi)) %>% 
+    unnest(authors) %>% 
+    filter(aff_id %in% ucd_ids) %>% 
+    filter(str_detect(aff_name, 'Department')) %>% 
+    count(aff_name) %>% 
+    arrange(desc(n))
+
+## Write output ----
+write_rds(pubs, str_c(data_dir, '02_pubs.Rds'))
+
